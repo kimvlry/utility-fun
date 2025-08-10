bc893f47bed478ef9dc5446a5dc136a2b93b2764
kimvlry <412963@niuitmo.ru>
2025-08-11 00:31:47 +0300

    feat: logger & response format

--- calendar/internal/http/middleware/logger.go
+++ calendar/internal/http/middleware/logger.go
@@ -1,1 +1,41 @@
 package middleware
+
+import (
+	"github.com/go-chi/chi/v5/middleware"
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+func NewHTTPMw(l *slog.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		l = l.With(
+			slog.String("component", "http/middleware"),
+		)
+		l.Info("starting http middleware")
+
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			entry := l.With(
+				slog.String("method", r.Method),
+				slog.String("path", r.URL.Path),
+				slog.String("request_id", middleware.GetReqID(r.Context())),
+				// TODO: more info?
+			)
+
+			// Response info wrapper
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+
+			startRequest := time.Now()
+			defer func() {
+				entry.Info("request completed. http middleware finished",
+					slog.Int("status", ww.Status()),
+					slog.Int("bytes written ", ww.BytesWritten()),
+					slog.String("duration", time.Since(startRequest).String()),
+				)
+			}()
+
+			next.ServeHTTP(w, r)
+		}
+		return http.HandlerFunc(fn)
+	}
+}
--- calendar/internal/lib/api/response/response.go
+++ calendar/internal/lib/api/response/response.go
@@ -1,1 +1,26 @@
 package response
+
+type Response struct {
+	Status string      `json:"status"`
+	Result interface{} `json:"result,omitempty"`
+	Error  string      `json:"error,omitempty"`
+}
+
+const (
+	StatusOK    = "OK"
+	StatusError = "Error"
+)
+
+func OK(result interface{}) Response {
+	return Response{
+		Status: StatusOK,
+		Result: result,
+	}
+}
+
+func Error(msg string) Response {
+	return Response{
+		Status: StatusError,
+		Error:  msg,
+	}
+}
